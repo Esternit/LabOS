3ac2dd0616146742d5f117bd1257ee50ce8b7560
Esternit <southonlyyt@gmail.com>
2025-11-19 11:21:27 +0300

    lab7

---
+++ lab7/reciever.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+
+#define SHM_NAME "/my_shm_time"
+#define SEM_NAME "/my_sem_time"
+#define BUF_SIZE 256
+
+int main()
+{
+    int shm_fd = shm_open(SHM_NAME, O_RDONLY, 0);
+    if (shm_fd == -1)
+    {
+        perror("shm_open (receiver)");
+        fprintf(stderr, "Error: Sender not running or shared memory not created.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *shm_ptr = mmap(NULL, BUF_SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+    if (shm_ptr == MAP_FAILED)
+    {
+        perror("mmap (receiver)");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_t *sem = sem_open(SEM_NAME, 0);
+    if (sem == SEM_FAILED)
+    {
+        perror("sem_open (receiver)");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t my_pid = getpid();
+    printf("Receiver started (PID: %d)\n", my_pid);
+
+    while (1)
+    {
+        if (sem_wait(sem) == -1)
+        {
+            perror("sem_wait");
+            break;
+        }
+
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        struct tm tm_info;
+        localtime_r(&ts.tv_sec, &tm_info);
+        char my_time[64];
+        strftime(my_time, sizeof(my_time), "%Y-%m-%d %H:%M:%S", &tm_info);
+
+        printf("[Receiver PID:%d | Time:%s] Received: %s\n", my_pid, my_time, shm_ptr);
+    }
+
+    munmap(shm_ptr, BUF_SIZE);
+    close(shm_fd);
+    sem_close(sem);
+    return 0;
+}
---
+++ lab7/sender.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+
+#define SHM_NAME "/my_shm_time"
+#define SEM_NAME "/my_sem_time"
+#define BUF_SIZE 256
+
+int main()
+{
+    sem_t *sem_check = sem_open(SEM_NAME "_lock", O_CREAT | O_EXCL, 0644, 1);
+    if (sem_check == SEM_FAILED)
+    {
+        perror("Another sender is already running or semaphore exists");
+        fprintf(stderr, "Error: Only one sender process allowed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1)
+    {
+        perror("shm_open");
+        exit(EXIT_FAILURE);
+    }
+    if (ftruncate(shm_fd, BUF_SIZE) == -1)
+    {
+        perror("ftruncate");
+        exit(EXIT_FAILURE);
+    }
+
+    char *shm_ptr = mmap(NULL, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shm_ptr == MAP_FAILED)
+    {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_t *sem = sem_open(SEM_NAME, O_CREAT, 0644, 1);
+    if (sem == SEM_FAILED)
+    {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t my_pid = getpid();
+    struct timespec ts;
+
+    printf("Sender started (PID: %d). Sending time every 2 seconds...\n", my_pid);
+
+    while (1)
+    {
+        clock_gettime(CLOCK_REALTIME, &ts);
+        char buf[BUF_SIZE];
+        struct tm tm_info;
+        localtime_r(&ts.tv_sec, &tm_info);
+        strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &tm_info);
+
+        snprintf(shm_ptr, BUF_SIZE, "PID:%d TIME:%s", my_pid, buf);
+
+        sem_post(sem);
+
+        sleep(2);
+    }
+
+    munmap(shm_ptr, BUF_SIZE);
+    close(shm_fd);
+    sem_close(sem);
+    sem_close(sem_check);
+    sem_unlink(SEM_NAME "_lock");
+    return 0;
+}
